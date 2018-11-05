d7401a26e86591f2afb38b7181d1a1fb71705822
Ismael <42499036+goismael@users.noreply.github.com>
2018-11-05 14:36:58 -0500

    Add files via upload

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 100;  counter++)
+            {
+               if (counter == 3 || counter == 5 || counter == 7 || counter == 2)
+                    Console.Write(" " + counter + " ");
+
+               else if ( counter % 5 ==0 || counter % 7==0 || counter % 2==0 || counter % 3==0 ) 
+                    Console.Write(" ");
+
+                else 
+                    Console.Write(" " + counter + " ");
+
+            
+            }
+            Console.Read();
+        }
+    }
+}
